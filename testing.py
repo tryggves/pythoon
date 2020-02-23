a789eef711b0d2f09dc7deaa15d275bb3cd873c8
tryggves <tryggve.sorensen@gmail.com>
2020-02-23 17:46:51 +0100

    Json load - works with 250MB file.

--- testing.py
+++ testing.py
@@ -1,5 +1,26 @@
-print("Hello world!")
+#!/usr/bin/python3
+import json
 
-x = 2
+my_pylist = {
+    "author" : "Tryggve Sorensen",
+    "age" : 24,
+    "results" : [1, 2, 2, 1]
+}
 
-print(x)
+my_parr = [ 25, 26, 27, 28 ]
+
+
+print(json.dumps(my_pylist, indent=2))
+print(json.dumps(my_parr, indent=2))
+
+with open("./commits.json", "r") as read_file:
+    data = json.load(read_file)
+
+print("Number of objects: ", len(data))
+count = 0
+for entry in data:
+    count = count+1
+    print("Entry num: ", count, "\tEntry length: ", len(entry))
+
+print(data[1])
+print("Done.")
